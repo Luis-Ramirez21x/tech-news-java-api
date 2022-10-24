f8b59adfc19d59c205f83703599266350c07db4e
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-10-23 21:43:35 -0400

    api tested and working, secuirty configuration added for local testing

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -2,8 +2,9 @@ package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +12,5 @@ public class TechNewsJavaApiApplication {
 	}
 
 }
+
+
